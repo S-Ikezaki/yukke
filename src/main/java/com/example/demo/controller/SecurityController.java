4388e1679a78c85f9b42fafa8dfde45e4a3126b8
unknown <181034>
2021-12-14 11:40:25 +0900

    コンペとゼミを分けて表示できるように・・なたよ

--- src/main/java/com/example/demo/controller/SecurityController.java
+++ src/main/java/com/example/demo/controller/SecurityController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.group.Group;
 import com.example.demo.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,10 +22,23 @@ public class SecurityController {
 
     @GetMapping("/")
     public String showMenu(Authentication loginUser, Model model) {
-        System.out.println(loginUser.getName());
+
+        List<Group> groupList = groupRepository.findAll();
+        List<Group> seminar = new ArrayList<Group>();
+        List<Group> competition = new ArrayList<Group>();
+
+        for (Group group: groupList){
+            if(group.getGroupRole() == 0) {
+                seminar.add(group);
+            } else {
+                competition.add(group);
+            }
+        }
+
         model.addAttribute("username", loginUser.getName());
         model.addAttribute("role", loginUser.getAuthorities());
-        model.addAttribute("groups",groupRepository.findAll());
+        model.addAttribute("seminars",seminar);
+        model.addAttribute("competitions",competition);
         return "main_menu";
     }
 
